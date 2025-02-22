d090b3f4e6920b77832133d2a9542a4e2616a1c9
Rakesh Singh <rakesh.s@skillsigma.com>
2025-02-22 12:31:18 +0530

    first commit

---
+++ src/layout/AuthLayout.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router";
+
+export default function AuthLayout() {
+  const token = localStorage.getItem('token');
+  const navigate = useNavigate();
+  useEffect(()=>{
+    if(token){
+      navigate('/dashboard')
+    }
+  },[token,navigate])
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+}
